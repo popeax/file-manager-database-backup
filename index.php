9138649511a39a01d2a2286c59de29f4714a15dc
selnomeria <selnomeria@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-06 15:48:43 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/file-manager-database-backup/trunk@1021003 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Plugin Name: File Browser, Manager and Backuper (+ Database)
+Description: View, Edit, Browser , Zip and Unzip files and folders.. Make Backups of files and databases + RESTORE them easily. (Built from https://github.com/tazotodua/Simple-PHP-file-browser-manager/ )
+Contributors: selnomeria, must@fa#
+Version: 0.1
+License: GPLv2
+*/
+
+//Wordpress File Manager & Browser
+
+
+add_action('admin_menu', 'wfmb_regist'); function wfmb_regist() {add_menu_page('File Manager and Backuper', 'File Manager and Backuper', 'administrator','wfmb-page', 'wfmb_output_func'); }
+function wfmb_output_func()
+{
+
+
+	$start_path = '/'.basename(dirname(dirname(dirname(dirname(__FILE__)))));
+	$tmp_filee	= substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, '7') .'pmp';
+
+	?>
+	
+	
+	<style>
+	.br_window_div	{	width: 100%;	height:100%;	}
+	iframe.fr_window{	width: 100%;	height:100%;	}
+	</style>
+
+	<div class="br_window_div">
+		<iframe onLoad="autoResize('ifrm1');" id="ifrm1" src="<?php echo plugins_url('',__FILE__); ?>/filemanager.php?do=login&path=.<?php echo $start_path;?>&temp_pass_file=<?php echo $tmp_filee;?>"  class="fr_window" scrolling="none"  frameborder="0"></iframe>
+	</div>
+	
+	<script language="JavaScript">
+	function autoResize(id)
+	{
+		var newheight;
+		if( document.getElementById(id) ){
+			newheight=document.getElementById(id).contentWindow.document.body.scrollHeight;
+		}
+		if (newheight < 400) {newheight = 400;}
+		document.getElementById(id).style.height= parseInt(newheight+40) + "px";
+	}
+	</script>
+
+
+
+	<?
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== File Browser, Manager and Backuper (+ Database) ===
+Contributors: selnomeria
+Tags: security, login, password, attack, hack, lock, authentication, auth, brute, force, brute force
+Requires at least: 3.0
+Tested up to: 3.5
+Stable tag: 0.1
+
+View, Edit, Browser , Zip and Unzip files and folders.. Make Backups of files and databases + RESTORE them easily.
+
+== Description ==
+
+(Built from https://github.com/tazotodua/Simple-PHP-file-browser-manager/ )
+
+== Installation ==
+
+1. Click "Add new" under plugins or Upload this Folder through FTP to `/wp-content/plugins/` directory.
+
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Frequently Asked Questions ==
+None.
+
+== Screenshots ==
+None.
+
+== Changelog ==
+
+= 0.1 =
+* First release edition.
+
+== Arbitrary section ==
+None.
